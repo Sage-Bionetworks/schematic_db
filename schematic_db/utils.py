bde4e11a0eefc231759854ac6a77bc0bcb158baa
andrewelamb <andrewelamb@gmail.com>
2024-02-13 11:33:39 -0800

    ran black

--- schematic_db/utils.py
+++ schematic_db/utils.py
@@ -2,6 +2,6 @@
 from typing import Literal
 
 
-#"display_label": use the display name as a label, if it is valid.
-#"class_label": use the class_label as a label
+# "display_label": use the display name as a label, if it is valid.
+# "class_label": use the class_label as a label
 DisplayLabelType = Literal["class_label", "display_label"]
